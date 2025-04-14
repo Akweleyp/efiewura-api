19fad8acdcbd0d89b61fe12e07808afcb8c146cb
Akweleyp <patriciaakweleyn@gmail.com>
2025-04-14 09:47:42 +0000

    server listening

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+
+// Create express app
+
+const app = express();
+
+
+
+// listening port 
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Server is Active on ${port}`);
+    
+})
