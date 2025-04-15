aa3b74856dd1243612ad560028d9a75d011b64e2
Akweleyp <patriciaakweleyn@gmail.com>
2025-04-15 17:23:25 +0000

    index.js changes

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 import express from "express";
+import cors from "cors";
+import listingRouter from "./router/listingroutes.js";
+import userRouter from './router/userroutes.js'
+                                 
+import mongoose from "mongoose";
+
+
+
+// database connection
+
+await mongoose.connect(process.env.MONGO_URI);
+
 
 // Create express app
 
 const app = express();
+
+// Use middlewares
+app.use(express.json());
+app.use(cors());
+
+//  Use Routers
+app.use(listingRouter);
+app.use(userRouter);
+
 
 // listening port
 const port = process.env.PORT || 4000;
